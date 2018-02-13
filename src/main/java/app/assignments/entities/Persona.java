2d8e7d4a2af7e2b02b865aa6a18d0f316d771ac1
Angel Alfaro <a3herrera@gmail.com>
2018-02-13 10:02:41 -0600

    Update Persona

--- src/main/java/app/assignments/entities/Persona.java
+++ src/main/java/app/assignments/entities/Persona.java
@@ -3,12 +3,12 @@ package app.assignments.entities;
 import java.util.Date;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
+@Data
 public class Persona {
 
 	private String nombre;
 	private String apellido;
 	private Date fechaNacimiento;
+	private long version;
 }
